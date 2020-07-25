224e27fe30f524c777494de50fd484e8675bf9f0
Siim Alas <siim.alas@outlook.com>
2020-07-25 10:03:58 +0300

    Removed unused JS and added preliminary annotation support.

--- BlazorUI/wwwroot/Javascript/Own/OpenSeadragonClient.js
+++ BlazorUI/wwwroot/Javascript/Own/OpenSeadragonClient.js
@@ -1,7 +1,7 @@
 
 window.OpenSeadragonClient = {
-    initDZI: function (viewerElement, tileSourcePaths) {
-        var viewer = OpenSeadragon({
+    initDZI: function (viewerElement, tileSourcePaths, annotations) {
+        this.viewer = OpenSeadragon({
             element: viewerElement,
             prefixUrl: "Javascript/dist/openseadragon-bin-2.4.2/images/",
             tileSources: tileSourcePaths,
@@ -9,5 +9,22 @@ window.OpenSeadragonClient = {
             showNavigator: true,
             ajaxWithCredentials: true
         });
+
+        this.annotations = annotations;
+
+        this.anno = OpenSeadragon.Annotorious(this.viewer, {
+            readOnly: true,
+            locale: 'auto'
+        });
+
+        this.anno.setAnnotations(this.annotations[0]);
+
+        var _this = this;
+        this.viewer.addHandler("page", function (e) {
+            _this.anno.setAnnotations(_this.annotations[e.page]);
+        });
+    },
+    addAnnotation: function () {
+        //
     }
 }
